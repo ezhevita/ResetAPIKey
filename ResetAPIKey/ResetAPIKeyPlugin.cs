0384514e90cf9473dca6a41def36be7678eedfbd
Vitaliya Chumakova <chumakova@mindbox.ru>
2022-05-21 17:02:09 +0300

    update username

--- ResetAPIKey/ResetAPIKeyPlugin.cs
+++ ResetAPIKey/ResetAPIKeyPlugin.cs
@@ -25,16 +25,10 @@ namespace ResetAPIKey {
 				.First(x => x.Key == "RepositoryUrl")
 				.Value ?? throw new InvalidOperationException(nameof(AssemblyMetadataAttribute));
 
-			const string git = ".git";
-			int index = repository.IndexOf(git, StringComparison.Ordinal);
-			if (index >= 0) {
-				repository = repository[..(index + 1)];
-			}
-
 			string company = assembly
 				.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? throw new InvalidOperationException(nameof(AssemblyCompanyAttribute));
 
-			ASF.ArchiLogger.LogGenericInfo(Name + " by " + company + " | Support & source code: " + repository);
+			ASF.ArchiLogger.LogGenericInfo(Name + " by ezhevita | Support & source code: https://github.com/ezhevita/ResetAPIKey");
 		}
 
 		public string Name => nameof(ResetAPIKey);
